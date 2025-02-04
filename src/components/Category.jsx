714020360a03da90f765b330efa14f5d91b2babf
shamsiddin <devshamsiddin@gmain.com>
2025-02-04 17:55:21 +0500

    change location of link

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,5 +1,6 @@
 import {FaArrowRight} from "react-icons/fa";
 import Card from "./Card";
+import { Link } from "react-router-dom";
 
 function Category({title, link, data}) {
   console.log(data.length);
@@ -8,9 +9,12 @@ function Category({title, link, data}) {
     <div className="container py-10">
       <div className="flex justify-between items-center mb-[50px]">
         <h1 className="text-2xl font-bold mb-4 ">{title} </h1>
-        <button className="text-[#017ba6] px-6 py-3 hover:bg-[#017ba6] hover:text-white  flex items-center gap-1 transition-all text-sm">
+        <Link
+          to={`/products`}
+          className="text-[#017ba6] px-6 py-3 hover:bg-[#017ba6] hover:text-white  flex items-center gap-1 transition-all text-sm"
+        >
           Смотреть все <FaArrowRight className="text-xs" />
-        </button>
+        </Link>
       </div>
       <div
         className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:${data.length > 4 ? "grid-cols-5" : "grid-cols-4"} gap-5 items-stretch `}>
